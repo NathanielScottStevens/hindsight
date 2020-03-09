d39a6f2f774a54fa233976c6fbd1324e53e035e3
Nathaniel Stevens <nstevens@pillartechnology.com>
2020-03-09 14:32:41 -0400

    Removes unused modules from define

--- apps/service_define/lib/define_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule DefineWeb.PageController do
-  use DefineWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- apps/service_define/test/service_define_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ServiceDefineWeb.PageControllerTest do
-  use ServiceDefineWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
