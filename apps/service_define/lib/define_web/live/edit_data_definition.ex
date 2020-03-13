3b797d21ad595093b35cfa90df5b488e4ddca0e0
Nathaniel Stevens <nathaniel.scott.stevens@gmail.com>
2020-03-13 16:32:37 -0400

    Got lists working

--- apps/service_define/lib/define_web/live/edit_data_definition.ex
+++ apps/service_define/lib/define_web/live/edit_data_definition.ex
@@ -5,8 +5,8 @@ defmodule DefineWeb.EditDataDefinition do
   import DefineWeb.ErrorHelpers
 
   def mount(_params, _session, socket) do
-    data_definition = %DataDefinition{}
-    changeset = DataDefinition.create()
+    data_definition = %DataDefinition{extract_steps: ["1", "2"]}
+    changeset = DataDefinition.update(data_definition)
 
     {:ok, assign(socket, data_definition: data_definition, changeset: changeset)}
   end
@@ -14,6 +14,7 @@ defmodule DefineWeb.EditDataDefinition do
   def render(assigns) do
     ~L"""
     <div>
+
       <h1>Data Definition</h1>
       <%= f = form_for @changeset, "#", [phx_change: :validate, phx_submit: :save] %>
         <%= label f, :dataset_id %>
@@ -28,6 +29,12 @@ defmodule DefineWeb.EditDataDefinition do
         <%= label f, :extract_destination %>
         <%= text_input f, :extract_destination %>
         <%= error_tag f, :extract_destination %>
+
+        <h4>Steps</h4>
+        <%= for step <- input_value(f, :extract_steps) || [""] do %>
+          <%= text_input f, :extract_steps, value: step, name: "data_definition[extract_steps][]" %>
+        <% end %>
+        <button phx-click="add_extract_step">Add Extract Step</button>
 
         <h2>Persistence</h2>
         <%= label f, :persist_source %>
@@ -49,6 +56,7 @@ defmodule DefineWeb.EditDataDefinition do
       socket.assigns.data_definition
       |> DataDefinition.update(params)
       |> Map.put(:action, :update)
+      |> IO.inspect(label: "lib/define_web/live/edit_data_definition.ex:56") 
 
     {:noreply, assign(socket, changeset: changeset)}
   end
@@ -57,5 +65,17 @@ defmodule DefineWeb.EditDataDefinition do
     DataDefinition.update(socket.assigns.data_definition, params)
 
     {:noreply, socket}
+  end
+
+  def handle_event("add_extract_step", _, socket) do
+    extract_steps = Map.get(socket.assigns.changeset.changes, :extract_steps, socket.assigns.data_definition.extract_steps)
+
+    changeset =
+      socket.assigns.data_definition
+      |> DataDefinition.update(%{extract_steps: extract_steps ++ [""]})
+      |> Map.put(:action, :update)
+      |> IO.inspect(label: "lib/define_web/live/edit_data_definition.ex:56") 
+
+    {:noreply, assign(socket, changeset: changeset)}
   end
 end
